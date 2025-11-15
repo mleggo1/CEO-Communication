dffa4439c983983b26f146ed266f0338febd7fb7
Michael Leggo <mleggo1@hotmail.com>
2025-11-15 15:32:28 +0800

    build: output to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 👇 This tells GitHub Pages the app is served under /CEO-Communication/
+  base: '/CEO-Communication/',
+  build: {
+    // 👇 Put the built files into "docs" (GitHub Pages can use this)
+    outDir: 'docs',
+  },
 })
